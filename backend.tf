66ee8021a5f8d6124a152a7298cde85189621b20
Botsky123 <104427127+Botsky123@users.noreply.github.com>
2024-02-23 09:42:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,5 +4,8 @@ terraform {
     storage_account_name = "ardhra1277"
     container_name       = "tfstate07"
     key                  = "terraform.tfstate"
+
+    subscription_id      = "27c2e3f9-b40e-4da9-95d8-ebfda85f9de6"
+    tenant_id            = "53ab52e3-5fa3-46d3-9fea-246af6320418"
   }
 }
